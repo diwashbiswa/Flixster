c4b664e7068e5e5c59fdfaa8e2fc0106ea4e0f75
kbiswa <kamal.biswa@wsu.edu>
2019-10-19 18:47:54 -0700

    landscape orientation mode added

--- app/src/main/java/com/example/flixster/Adapter/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/Adapter/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.Adapter;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -61,7 +62,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+            String imageURL;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                imageURL = movie.getBackdropPath();
+            } else {
+                imageURL = movie.getPosterPath();
+            }
+            Glide.with(context).load(imageURL).into(ivPoster);
         }
     }
 }
